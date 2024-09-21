98c3a57c458347e244f4d3f650296265e411aa0a
lalekan <lekan.suleiman@gmail.com>
2024-09-20 17:38:56 -0700

    created app model

--- models/user.js
+++ models/user.js
@@ -1,5 +1,29 @@
 const mongoose = require('mongoose');
 
+// models/user.js
+
+const applicationSchema = new mongoose.Schema({
+  company: {
+    type: String,
+    required: true,
+  },
+  title: {
+    type: String,
+    required: true,
+  },
+  notes: {
+    type: String,
+  },
+  postingLink: {
+    type: String,
+  },
+  status: {
+    type: String,
+    enum: ['interested', 'applied', 'interviewing', 'rejected', 'accepted'],
+  },
+});
+
+
 const userSchema = mongoose.Schema({
   username: {
     type: String,
@@ -9,7 +33,10 @@ const userSchema = mongoose.Schema({
     type: String,
     required: true,
   },
+  applications: [applicationSchema]
 });
+
+
 
 const User = mongoose.model('User', userSchema);
 
